ddfca6a3254739a1e92220e65aa3e65debb356fe
ser1888 <ser1888kg@gmail.com>
2021-06-01 03:43:49 +0600

    added: add a new todo; custom hook

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import TodoList from './Todo/TodoList'
 import React from 'react'
 import Context from './Context'
+import TodoList from './Todo/TodoList'
+import AddTodo from './Todo/AddTodo'
 
 function App() {
   const [todos, setTodos] = React.useState([
@@ -25,10 +26,22 @@ function App() {
       todos.filter(todo => todo.id !== id)
     )
   }
+
+  function addTodo(title) {
+    setTodos(
+      todos.concat([{
+        title,
+        id: Date.now(),
+        completed: false 
+      }])
+    )
+  }
+
   return (
     <Context.Provider value={{ removeTodo }}>    
     <div className="wrapper">
       <h1>REACT TUTORIAL</h1>
+      <AddTodo onCreate={addTodo}></AddTodo>
       {todos.length ? <TodoList todos={todos} onToggle={toggleTodo}/> : <p>No todos</p>}
       
     </div>
